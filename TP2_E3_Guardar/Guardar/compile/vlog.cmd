b83fd6f2c18173398e78bccc37c192ae057232ca
agalan203 <83973836+agalan203@users.noreply.github.com>
2022-11-02 10:44:18 -0300

    Modulo Guardar Terminado

---
+++ TP2_E3_Guardar/Guardar/compile/vlog.cmd
@@ -0,0 +1,1 @@
+-o c:/My_Designs/Guardar_TP2_E3/Guardar/compile -lc c:/My_Designs/Guardar_TP2_E3/library.cfg -fl c:/My_Designs/Guardar_TP2_E3/Guardar/projlib.cfg -pli "C:/Aldec/Active-HDL-Student-Edition/bin/systf.dll" -epr c:/My_Designs/Guardar_TP2_E3/Guardar/compile/Guardar.epr -O2 -sve -msg 5 -sv2k12 -work Guardar c:/My_Designs/Guardar_TP2_E3/Guardar/src/guardar.v 
---
+++ TP2_E3_Guardar/Guardar/src/guardar.v
@@ -0,0 +1,73 @@
+module Save(
+    input wire [3:0] nr,
+    input wire [1:0] regi,
+    input wire reset,
+    output reg [15:0] reg1,
+    output reg [15:0] reg2,
+    output reg regop
+    );
+
+    always @(nr or regi or reset)
+        begin
+            regop = 0;
+            if (reset==1)
+                begin
+                    reg1 = 0;
+                    reg2 = 0;
+                end
+            else
+                begin
+                    case(regi)
+                        0 : begin
+                                reg1[15:12] = reg1[11:8];
+                                reg1[11:8] = reg1[7:4];
+                                reg1[7:4] = reg1[3:0];
+                                reg1[3:0] = nr;
+                            end
+                        1 : begin
+                                reg2[15:12] = reg2[11:8];
+                                reg2[11:8] = reg2[7:4];
+                                reg2[7:4] = reg2[3:0];
+                                reg2[3:0] = nr;
+                            end
+                        2 : if (nr==4'b1010) regop = 1;
+                    endcase
+                end
+        end
+endmodule
+
+
+module TestSave();
+
+    reg [3:0] nr;
+    reg [1:0] regi;
+    reg reset;
+    reg [15:0] reg1;
+    reg [15:0] reg2;
+    reg regop;
+
+    Save my_save(.nr(nr), .regi(regi), .reset(reset), .reg1(reg1), .reg2(reg2), .regop(regop));
+
+    initial
+        begin
+			assign reset = 1;
+            #5ns
+            assign nr = 4'b0011;
+            assign regi = 0;
+            assign reset = 0;
+            #15ns
+            assign nr = 4'b0101;
+            assign regi = 0;
+            assign reset = 0;
+            #15ns 
+            assign nr = 4'b0001;
+            assign regi = 1;
+            assign reset = 0;
+            #15ns 
+            assign reset = 1;
+			#15ns 
+            assign nr = 4'b1010;
+            assign regi = 2;
+            assign reset = 0;
+        end
+endmodule
